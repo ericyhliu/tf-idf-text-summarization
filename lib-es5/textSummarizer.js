b15db09e9b0288daaa9d5a06af21494c1f5930f5
Eric Liu <ericliu314@gmail.com>
2017-09-06 23:56:33 -0400

    Add textSummarizer.js

--- lib-es5/textSummarizer.js
+++ lib-es5/textSummarizer.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -20,14 +20,135 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  * 
  */
 
+var fs = require('fs');
+var natural = require('natural');
+var Tokenizer = require('sentence-tokenizer');
+
+var _require = require('./porterStemmer'),
+    PorterStemmer = _require.PorterStemmer;
+
 var TextSummarizer = function () {
     function TextSummarizer() {
         _classCallCheck(this, TextSummarizer);
     }
 
-    _createClass(TextSummarizer, [{
-        key: "summarize",
-        value: function summarize() {}
+    _createClass(TextSummarizer, null, [{
+        key: '_isNumber',
+        value: function _isNumber(n) {
+            return !isNaN(parseFloat(n)) && isFinite(n);
+        }
+    }, {
+        key: '_convertToVector',
+        value: function _convertToVector(slist) {
+            var vector = {};
+            slist.forEach(function (s) {
+                if (!vector[s]) {
+                    vector[s] = {
+                        count: 1
+                    };
+                } else {
+                    vector[s].count++;
+                }
+            });
+            return vector;
+        }
+    }, {
+        key: 'summarize',
+        value: function summarize(document, threshold) {
+            var wordTokenizer = new natural.WordTokenizer();
+            var documentTokenizer = new Tokenizer();
+            documentTokenizer.setEntry(document);
+
+            // Convert document into vector, normalize document tokens:
+            var docWords = wordTokenizer.tokenize(document);
+            docWords = docWords.filter(function (w) {
+                return w.match(/[a-z]/i) && !TextSummarizer._isNumber(w);
+            }).map(function (w) {
+                return PorterStemmer.stem(w.toLowerCase());
+            });
+
+            var docVector = TextSummarizer._convertToVector(docWords);
+
+            // Tokenize document by sentence, convert each sentence into a vector,
+            // normalize sentence tokens:
+            var sentVectors = documentTokenizer.getSentences().map(function (s, i) {
+                return {
+                    original: s,
+                    vector: TextSummarizer._convertToVector(wordTokenizer.tokenize(s).filter(function (w) {
+                        return w.match(/[a-z]/i) && !TextSummarizer._isNumber(w);
+                    }).map(function (w) {
+                        return PorterStemmer.stem(w.toLowerCase());
+                    })),
+                    index: i
+                };
+            });
+
+            // For each term of each vector of each sentence, add the log 
+            // frequency weighting of the term frequency (tf):
+            sentVectors.forEach(function (sv) {
+                var keys = Object.keys(sv.vector);
+                sv.keys = keys;
+                keys.forEach(function (k) {
+                    sv.vector[k].weight = 1 + Math.log10(sv.vector[k].count);
+                });
+            });
+
+            // Build the inverse document frequency (idf) weighting, and add 
+            // tf-idf score to each term in the document:
+            var docVectorKeys = Object.keys(docVector);
+            var N = docVectorKeys.length;
+
+            var idf = {};
+            docVectorKeys.forEach(function (k) {
+                idf[k] = 0;
+                sentVectors.forEach(function (sv) {
+                    if (sv.vector[k]) {
+                        idf[k]++;
+                    }
+                });
+                idf[k] = Math.log10(N / idf[k]);
+                docVector[k].tfIdf = (1 + Math.log10(docVector[k].count)) * idf[k];
+            });
+
+            // Add tf-idf score of each term in each sentence, and the cosine 
+            // similarity between the tf-idf scores of each term of each sentence 
+            // vector and that of the document vector:
+            var docNorm = Math.sqrt(docVectorKeys.map(function (k) {
+                return docVector[k].tfIdf;
+            }).reduce(function (acc, x) {
+                return acc + Math.pow(x, 2);
+            }, 0));
+
+            sentVectors.forEach(function (sv) {
+                var svNorm = 0;
+                var dotProd = 0;
+                sv.keys.forEach(function (k) {
+                    sv.vector[k].tfIdf = sv.vector[k].weight * idf[k];
+                    dotProd += sv.vector[k].tfIdf * docVector[k].tfIdf;
+                    svNorm += Math.pow(sv.vector[k].tfIdf, 2);
+                });
+                svNorm = Math.sqrt(svNorm);
+
+                if (svNorm) {
+                    sv.theta = Math.acos(dotProd / (svNorm * docNorm));
+                    sv.score = (Math.acos(0) - sv.theta) / Math.acos(0);
+                } else {
+                    sv.theta = 0;
+                    sv.score = 0;
+                }
+            });
+
+            // Build summarized version of text by filtering all sentence vectors 
+            // with scores that are greater or equal to the threshold
+            // (between 0 - 1):
+            var summary = sentVectors.filter(function (sv) {
+                return sv.score >= threshold;
+            }).map(function (sv) {
+                return sv.original;
+            }).join('\n');
+
+            return summary;
+        }
     }]);
 
     return TextSummarizer;
--- lib-es6/textSummarizer.js
+++ lib-es6/textSummarizer.js
@@ -14,12 +14,139 @@
  * 
  */
 
+const fs = require('fs');
+const natural = require('natural');
+const Tokenizer = require('sentence-tokenizer');
+const { stemmer } = require('porter-stemmer');
+
 class TextSummarizer {
 
-    constructor() {}
+    static _isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
 
-    summarize() {
+    static _convertToVector(slist) {
+        let vector = {};
+        slist.forEach((s) => {
+            if (!vector[s]) {
+                vector[s] = {
+                    count: 1
+                };
+            } else {
+                vector[s].count++;
+            }
+        });
+        return vector;
+    }
 
+    static summarize(document, threshold) {
+        let wordTokenizer = new natural.WordTokenizer();
+        let documentTokenizer = new Tokenizer();
+        documentTokenizer.setEntry(document);
+
+        // Convert document into vector, normalize document tokens:
+        let docWords = wordTokenizer.tokenize(document);
+        docWords = docWords
+        .filter((w) => {
+            return w.match(/[a-z]/i) && !TextSummarizer._isNumber(w);
+        })
+        .map((w) => {
+            return stemmer(w.toLowerCase());
+        });
+    
+        let docVector = TextSummarizer._convertToVector(docWords);
+
+        // Tokenize document by sentence, convert each sentence into a vector,
+        // normalize sentence tokens:
+        let sentVectors = documentTokenizer.getSentences()
+        .map((s, i) => {
+            return {
+                original: s,
+                vector: TextSummarizer._convertToVector(
+                    wordTokenizer
+                    .tokenize(s)
+                    .filter((w) => {
+                        return w.match(/[a-z]/i) && !TextSummarizer._isNumber(w);
+                    })
+                    .map((w) => {
+                        return stemmer(w.toLowerCase());
+                    })),
+                index: i
+            };
+        });
+
+        // For each term of each vector of each sentence, add the log 
+        // frequency weighting of the term frequency (tf):
+        sentVectors.forEach((sv) => {
+            const keys = Object.keys(sv.vector);
+            sv.keys = keys;
+            keys.forEach((k) => {
+                sv.vector[k].weight = 1 + Math.log10(sv.vector[k].count)
+            });
+        });
+
+        // Build the inverse document frequency (idf) weighting, and add 
+        // tf-idf score to each term in the document:
+        const docVectorKeys = Object.keys(docVector);
+        const N = docVectorKeys.length;
+
+        let idf = {};
+        docVectorKeys.forEach((k) => {
+            idf[k] = 0;
+            sentVectors.forEach((sv) => {
+                if (sv.vector[k]) {
+                    idf[k]++;
+                }
+            });
+            idf[k] = Math.log10(N/idf[k]);
+            docVector[k].tfIdf =  (1 + Math.log10(docVector[k].count)) * 
+                idf[k];
+        });
+
+        // Add tf-idf score of each term in each sentence, and the cosine 
+        // similarity between the tf-idf scores of each term of each sentence 
+        // vector and that of the document vector:
+        let docNorm = Math.sqrt(docVectorKeys
+        .map((k) => {
+            return docVector[k].tfIdf;
+        })
+        .reduce((acc, x) => {
+            return acc + Math.pow(x, 2);
+        }, 0));
+
+        sentVectors.forEach((sv) => {
+            let svNorm = 0;
+            let dotProd = 0;
+            sv.keys.forEach((k) => {
+                sv.vector[k].tfIdf = sv.vector[k].weight * idf[k];
+                dotProd += sv.vector[k].tfIdf * docVector[k].tfIdf;
+                svNorm += Math.pow(sv.vector[k].tfIdf, 2);
+            });
+            svNorm = Math.sqrt(svNorm);
+            
+            if (svNorm) {
+                sv.theta = Math.acos(dotProd / (svNorm * docNorm));
+                sv.score = (Math.acos(0) - sv.theta) / Math.acos(0);
+            } else {
+                sv.theta = 0;
+                sv.score = 0;
+            }
+        });
+
+        // Build summarized version of text by filtering all sentence vectors 
+        // with scores that are greater or equal to the threshold
+        // (between 0 - 1):
+        let summary = sentVectors
+        .filter((sv) => {
+            return sv.score >= threshold;
+        })
+        .map((sv) => {
+            return sv.original;
+        })
+        .join('\n');
+        
+        // Return summarized version of the text:
+        return summary;
     }
 }
 
